4da4ed64987913440f9ac2b14d23ad0bac1cb208
Ryan Pavlik <rpavlik@iastate.edu>
2011-11-30 09:34:59 -0600

    Add desktop haption config

---
+++ Devices/Haption New Bubble Desktop VR.lua
@@ -0,0 +1,25 @@
+
+haption = Manipulators.Haption.Virtuose6D3545{
+	ipaddress = "192.168.1.13",
+	scale = 1.0,
+	hardwareForceScale = 1.0,
+	torqueScale = 1.0,
+	forces = true,
+	indexType = "INDEXING_NONE"
+}
+
+nav = Navigation()
+myBubble = TranslationalBubble(0.15, 100, .5, TranslationalBubble.PeakRingVelocity)
+
+bubble = BubbleNav{
+	bubble = myBubble,
+	manipulator = haption,
+	position = {.20, 1.0, -.35}, -- starting position for bubble
+	elasticForce = true,
+	deviceCenter =  {0, 0.01678, 0.6},
+	showBubble = false,
+	navigate = true,
+	navigator = nav
+}
+
+addManipulator(nav:transformManipulator(bubble))
