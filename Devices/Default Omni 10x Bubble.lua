7f5555b0140ae62f37fb8b5644d83eb90d885f9c
Ryan Pavlik <rpavlik@iastate.edu>
2011-12-02 11:50:03 -0600

    Add an omni configuration

---
+++ Devices/Default Omni 10x Bubble.lua
@@ -0,0 +1,20 @@
+
+omni = Manipulators.Sensable.PhantomOmni{
+	forces = true,
+	scale = 10.0 -- default is 20
+}
+
+--This is the "old" method, the new method is BubbleNav
+bubble = BubbleTechnique{
+	manipulator = omni,
+	radius = 0.45, -- radius of bubble in meters
+	stiffness = 50.0, -- bubble stiffness
+	bubbleVelocityScale = 7.0, -- default of 3.0
+	position = {1.5, 1.35, 0.25}, -- starting position for bubble
+	elasticForce = true,
+	deviceCenter = {-0.04837, 0.4352, 0.20},
+	showBubble = false,
+	navigate = {true, false, true} -- navigate in x and z only
+}
+
+addManipulator(bubble)
