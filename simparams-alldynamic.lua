03f22214c72a13d50e8c738e122d201f2b985886
Ryan Pavlik <rpavlik@iastate.edu>
2011-12-08 12:39:35 -0600

    Add an "alldynamic" simparam

---
+++ simparams-alldynamic.lua
@@ -0,0 +1,12 @@
+-- Set up simulation parameters
+defineSimulationParameters{
+	-- maxStiffness - set based on the device you're using:
+	-- 300 is good for the omni.
+	maxStiffness = 300.0,
+
+	-- allBodiesAreDynamic - if you specify this as true,
+	-- then Newton's third law is in effect. If you don't
+	-- specify this, it defaults to false: object you aren't
+	-- moving don't move at all
+	allBodiesAreDynamic = true
+}
