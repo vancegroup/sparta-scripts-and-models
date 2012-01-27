f3159007d1c43e0b0ea2eb136def38630db774f4
Patrick Carlson <carlsonp@iastate.edu>
2012-01-26 18:44:53 -0600

    add custom graphics parameter to subassembly

--- Subassembly/keyboard_subassembly_burr_puzzle.lua
+++ Subassembly/keyboard_subassembly_burr_puzzle.lua
@@ -1,5 +1,6 @@
 --Keyboard subassembly implementation
 require("Actions")
+require("osgFX")
 --The user should be setting this to the appropriate haptics-lab-jconf file location
 vrjKernel.loadConfigFile("C:/Users/carlsonp/Desktop/src/haptics-lab-jconf/fishbowl/mixins/Subassembly_Keyboard.jconf")
 subBtn = gadget.DigitalInterface("VJSubassembly")
@@ -95,7 +96,7 @@ function UserEnterExit()
 	end
 end
 
-lastButtonState = false
+local lastButtonState = false
 function UserAddRemove()
 	if right:getButtonState(1) and right.hovering and lastButtonState == false then
 		lastButtonState = true
@@ -108,6 +109,11 @@ function UserAddRemove()
 	end
 end
 
-StartSubassembly(UserEnterExit, UserAddRemove)
+--This defines graphically what will change when items
+--are added/removed from the subassembly.
+local GraphicsNode = osgFX.Scribe()
+GraphicsNode:setWireframeLineWidth(20)
+
+StartSubassembly(UserEnterExit, UserAddRemove, GraphicsNode)
 
 simulation:startInSchedulerThread()
