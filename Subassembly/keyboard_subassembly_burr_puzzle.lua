d5c3d1f048de37ae1ee9571db7b39d8ea10b9628
Patrick Carlson <carlsonp@iastate.edu>
2012-01-27 13:29:55 -0600

    use split out scripts for loading in models and devices

--- Subassembly/keyboard_subassembly_burr_puzzle.lua
+++ Subassembly/keyboard_subassembly_burr_puzzle.lua
@@ -1,6 +1,10 @@
 --Keyboard subassembly implementation
 require("Actions")
 require("osgFX")
+
+require("getScriptFilename")
+vrjLua.appendToModelSearchPath(getScriptFilename())
+
 --The user should be setting this to the appropriate haptics-lab-jconf file location
 vrjKernel.loadConfigFile("C:/Users/carlsonp/Desktop/src/haptics-lab-jconf/fishbowl/mixins/Subassembly_Keyboard.jconf")
 subBtn = gadget.DigitalInterface("VJSubassembly")
@@ -9,83 +13,9 @@ params = defineSimulationParameters{
 	maxStiffness = 300.0
 }
 
-voxSize = 0.003
-densitySize = 20
-scaleSize = 4.0
+dofile(vrjLua.findInModelSearchPath("burr_models.lua"))
 
-baseblock = addObject{
-	voxelsize = voxSize,
-	position = {0.0, 1.0, -0.5},
-	density = densitySize,
-	Transform{
-		scale = scaleSize,
-		Model("V:/Applications/Vances_group/GOALI/Burr/BaseBlock.ive")
-	}
-}
-block1 = addObject{
-	voxelsize = voxSize,
-	position = {0.3, 1.0, -0.5},
-	density = densitySize,
-	Transform{
-		scale = scaleSize,
-		Model("V:/Applications/Vances_group/GOALI/Burr/Block1.ive")
-	}
-}
-block2 = addObject{
-	voxelsize = voxSize,
-	position = {0.6, 1.0, -0.5},
-	density = densitySize,
-	Transform{
-		scale = scaleSize,
-		Model("V:/Applications/Vances_group/GOALI/Burr/Block2.ive")
-	}
-}
-block3 = addObject{
-	voxelsize = voxSize,
-	position = {0.0, 1.2, -0.5},
-	density = densitySize,
-	Transform{
-		scale = scaleSize,
-		Model("V:/Applications/Vances_group/GOALI/Burr/Block3.ive")
-	}
-}
-block4 = addObject{
-	voxelsize = voxSize,
-	position = {0.3, 1.2, -0.5},
-	density = densitySize,
-	Transform{
-		scale = scaleSize,
-		Model("V:/Applications/Vances_group/GOALI/Burr/Block4.ive")
-	}
-}
-block5 = addObject{
-	voxelsize = voxSize,
-	position = {0.6, 1.2, -0.5},
-	density = densitySize,
-	Transform{
-		scale = scaleSize,
-		Model("V:/Applications/Vances_group/GOALI/Burr/Block5.ive")
-	}
-}
-
---Add Omni
-manip = Manipulators.Sensable.PhantomOmni{
-	name = "Omni2",
-	forces = true,
-	scale = 8.0
-}
-right = translateManipulator{
-	manip,
-	translation = {0.6, 1.0, -0.5}
-}
-addManipulator(right)
-left = Manipulators.Gadgeteer.Glove{position = "LeftGlove",
-	options = "USB1",
-	hardware = "GloveHardware5DT",
-	reportType = "KalmanFilter",  -- "KalmanFilter", "Raw", "HardwareCalibrated", "GloveToolsCalibrated"
-	calibFile = "assets/calibrations/calib-left-5dt.txt",
-}
-addManipulator(left)
+dofile(vrjLua.findInModelSearchPath("load_devices.lua"))
 
 function UserEnterExit()
 	--wait until key is pressed to go into edit mode
