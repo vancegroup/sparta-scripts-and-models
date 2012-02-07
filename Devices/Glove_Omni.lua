fa6eb233481a53ba095fbb7191901c3a7314d295
Patrick Carlson <carlsonp@iastate.edu>
2012-02-07 10:19:53 -0600

    add omni/glove device setup script

---
+++ Devices/Glove_Omni.lua
@@ -0,0 +1,18 @@
+--Add Omni
+manip = Manipulators.Sensable.PhantomOmni{
+	name = "Omni2",
+	forces = true,
+	scale = 4.0
+}
+right = translateManipulator{
+	manip,
+	translation = {0.6, 1.0, -0.5}
+}
+addManipulator(right)
+left = Manipulators.Gadgeteer.Glove{position = "LeftGlove",
+	options = "USB1",
+	hardware = "GloveHardware5DT",
+	reportType = "KalmanFilter",  -- "KalmanFilter", "Raw", "HardwareCalibrated", "GloveToolsCalibrated"
+	calibFile = "assets/calibrations/calib-left-5dt.txt",
+}
+addManipulator(left)
