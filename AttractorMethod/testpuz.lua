f2439a3dd7a4eb91d2c682f44ebcc76b136f6140
Leif Berg <lpberg@iastate.edu>
2012-05-04 16:55:26 -0500

    replaced attractor calls to new attractor group

--- AttractorMethod/testpuz.lua
+++ AttractorMethod/testpuz.lua
@@ -101,12 +101,17 @@ red = addObject{
 	pieceRed,
 }
 
-addAttractorForce{body1 = green, body2 = yellow, forceDist = .2, snapDist = .025, stiff = 250, }
-addAttractorForce{body1 = blue, body2 = yellow, forceDist = .2, snapDist = .025, stiff = 250, }
-addAttractorForce{body1 = blue, body2 = purple, forceDist = .2, snapDist = .025, stiff = 250, }
-addAttractorForce{body1 = red, body2 = yellow, forceDist = .2, snapDist = .025, stiff = 250, }
-addAttractorForce{body1 = teal, body2 = yellow, forceDist = .2, snapDist = .025, stiff = 250, }
-
+AttractorGroup{
+	red,
+	blue,
+	yellow, 
+	purple,
+	teal,
+	green,
+	forceDist = .2, 
+	snapDist = .025, 
+	stiff = 250
+}
 
 
 omni = Manipulators.Sensable.PhantomOmni{
