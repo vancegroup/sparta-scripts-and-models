d3b581654e7190d163547000059b9b64a7648b69
Ryan Pavlik <rpavlik@iastate.edu>
2012-06-13 14:23:59 -0500

    Add scripts for full demos

---
+++ FullDemos/wand-metal-demo.lua
@@ -0,0 +1,27 @@
+require("getScriptFilename")
+vrjLua.appendToModelSearchPath(getScriptFilename())
+function runfile(fn)
+	local fullpath = vrjLua.findInModelSearchPath(fn)
+	if fullpath ~= "" then
+		print([[Running "]] .. fullpath .. [["...]])
+		dofile(fullpath)
+	else
+		error("Couldn't find " .. fn .. " in model search path!", 2)
+	end
+end
+
+-- Scale huge for the video
+--relativeScale = 1.0
+
+-- Scale smaller for a demo
+relativeScale = .8
+
+
+bracketModelScale = 0.6 * relativeScale
+partDensity = 20 / relativeScale
+
+runfile("../simparams.lua")
+runfile("../UBracket/bracket_models.lua")
+runfile("../Devices/METaL Bubble Wand.lua")
+runfile("../homebutton-toggles-bubble.lua")
+runfile("../start-sim.lua")
