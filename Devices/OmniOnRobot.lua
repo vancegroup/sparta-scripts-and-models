51a2a8be067c8c56bec9971e3f31ae3aae7b4c86
Ryan Pavlik <rpavlik@iastate.edu>
2012-08-08 19:11:28 -0500

    Add a commented line about gain

--- Devices/OmniOnRobot.lua
+++ Devices/OmniOnRobot.lua
@@ -5,5 +5,7 @@ omni = Manipulators.Sensable.PhantomOmni{
 
 remote = VRPNRemoteBase(omni, 0, 0, "ErrorCommand@localhost")
 
+-- you can run the following command (and similar ones) to change the computer-side gain at runtime.
+remote.gain = 1
 
 addManipulator(remote)
