c05d08b8a8c7546a1f43c39d42bbc6e9b0c20119
Ryan Pavlik <rpavlik@iastate.edu>
2012-10-26 14:59:53 -0500

    Fix speed of wand bubble in METaL

--- Devices/METaL Bubble Wand.lua
+++ Devices/METaL Bubble Wand.lua
@@ -2,12 +2,12 @@
 wand = Manipulators.Gadgeteer.Wand{position = "VJWand"}
 bubble = BubbleTechnique{
 	manipulator = wand,
-	radius = .875, -- radius of bubble in meters
+	radius = 1, -- radius of bubble in meters
 	stiffness = 100.0, -- bubble stiffness
-	bubbleVelocityScale = 50.0, -- default of 3.0
-	position = {2, 0, 1.5}, -- starting position for bubble
+	bubbleVelocityScale = 15.0, -- default of 3.0
+	position = {2.25, 0, 1.5}, -- starting position for bubble
 	elasticForce = true,
-	deviceCenter = {2, 1.5, 1.5},
+	deviceCenter = {2.25, 1.5, 1.5},
 	showBubble = true,
 	lockedToFloor = true,
 	navigate = {true, false, true} -- navigate in x and z only
