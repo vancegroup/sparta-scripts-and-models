92f7c36b7796f7a7ed52611b26a7e952354c46ab
Patrick Carlson <carlsonp@iastate.edu>
2013-04-10 10:38:29 -0500

    split out loading devices into functions

--- Subassembly/load_devices.lua
+++ Subassembly/load_devices.lua
@@ -1,18 +1,67 @@
---Add Omni
-manip = Manipulators.Sensable.PhantomOmni{
-	name = "Omni2",
-	forces = true,
-	scale = 4.0
-}
-right = translateManipulator{
-	manip,
-	translation = {0.6, 1.0, -0.5}
-}
-addManipulator(right)
-left = Manipulators.Gadgeteer.Glove{position = "LeftGlove",
-	options = "USB1",
-	hardware = "GloveHardware5DT",
-	reportType = "KalmanFilter", -- "KalmanFilter", "Raw", "HardwareCalibrated", "GloveToolsCalibrated"
-	calibFile = "assets/calibrations/calib-left-5dt.txt",
-}
-addManipulator(left)
+function rightOmni()
+	manip = Manipulators.Sensable.PhantomOmni{
+		name = "Omni2",
+		forces = true,
+		scale = 4.0
+	}
+	right = translateManipulator{
+		manip,
+		translation = {0.6, 1.0, -0.5}
+	}
+	addManipulator(right)
+end
+
+function leftOmni()
+	manip = Manipulators.Sensable.PhantomOmni{
+		name = "Omni1",
+		forces = true,
+		scale = 4.0
+	}
+	left = translateManipulator{
+		manip,
+		translation = {0.0, 1.0, -0.5}
+	}
+	addManipulator(left)
+end
+
+function rightGlove()
+	right = Manipulators.Gadgeteer.Glove{position = "RightGlove",
+		options = "USB0",
+		hardware = "GloveHardware5DT",
+		reportType = "KalmanFilter", -- "KalmanFilter", "Raw", "HardwareCalibrated", "GloveToolsCalibrated"
+		calibFile = "assets/calibrations/calib-right-5dt.txt",
+	}
+	addManipulator(right)
+end
+
+function leftGlove()
+	left = Manipulators.Gadgeteer.Glove{position = "LeftGlove",
+		options = "USB1",
+		hardware = "GloveHardware5DT",
+		reportType = "KalmanFilter", -- "KalmanFilter", "Raw", "HardwareCalibrated", "GloveToolsCalibrated"
+		calibFile = "assets/calibrations/calib-left-5dt.txt",
+	}
+	addManipulator(left)
+end
+
+
+if devices == "dualomni" then
+	rightOmni()
+	leftOmni()
+end
+
+if handedness == "left" then
+	if devices == "gloveomni" then
+		--Glove in right, Omni in left
+		rightGlove()
+		leftOmni()
+	end
+end
+
+if handedness == "right" then
+	if devices == "gloveomni" then
+		--Omni in right, Glove in left
+		rightOmni()
+		leftGlove()
+	end
+end
