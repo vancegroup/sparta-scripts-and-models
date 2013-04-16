8af2fa7a3e5185aa96c72e7df8fe28e53a41f2f7
Patrick Carlson <carlsonp@iastate.edu>
2013-04-16 13:03:38 -0500

    consolidated attractor (snap to fit) examples into one working example

---
+++ AttractorMethod/burr_puzzle_test.lua
@@ -0,0 +1,91 @@
+require("getScriptFilename")
+vrjLua.appendToModelSearchPath(getScriptFilename())
+params = defineSimulationParameters{
+	maxStiffness = 300.0
+}
+--devices - "dualomni" or "gloveomni" should be passed in via launcher
+--handedness - "left" or "right" should be passed in via launcher
+dofile(vrjLua.findInModelSearchPath("load_devices.lua"))
+
+local scale = 5.0
+
+--These Burr2 puzzle pieces have the default location of the original
+--Sketchup model to be in the correct position and orientation for the origin
+--as well as in an assembled state.
+Red = Transform{
+	scale = scale,
+	Model([[V:\Applications\vancegroup\GOALI\Burr2\Block1.ive]])
+}
+Blue = Transform{
+	scale = scale,
+	Model([[V:\Applications\vancegroup\GOALI\Burr2\Block2.ive]])
+}
+LightBlue = Transform{
+	scale = scale,
+	Model([[V:\Applications\vancegroup\GOALI\Burr2\Block3.ive]])
+}
+Purple = Transform{
+	scale = scale,
+	Model([[V:\Applications\vancegroup\GOALI\Burr2\Block4.ive]])
+}
+Green = Transform{
+	scale = scale,
+	Model([[V:\Applications\vancegroup\GOALI\Burr2\Block5.ive]])
+}
+Yellow = Transform{
+	scale = scale,
+	Model([[V:\Applications\vancegroup\GOALI\Burr2\Block6.ive]])
+}
+
+local pos = {.3, .8, -.5}
+
+blue = addObject{
+	position = pos,
+	voxelsize = 0.003,
+	density = 5,
+	Blue,
+}
+yellow = addObject{
+	position = pos,
+	voxelsize = 0.003,
+	density = 5,
+	Yellow,
+}
+green = addObject{
+	position = pos,
+	voxelsize = 0.003,
+	density = 5,
+	Green,
+}
+purple = addObject{
+	position = pos,
+	voxelsize = 0.003,
+	density = 5,
+	Purple,
+}
+teal = addObject{
+	position = pos,
+	voxelsize = 0.003,
+	density = 5,
+	LightBlue,
+}
+red = addObject{
+	position = pos,
+	voxelsize = 0.003,
+	density = 5,
+	Red,
+}
+
+AttractorGroup{
+	red,
+	blue,
+	yellow, 
+	purple,
+	teal,
+	green,
+	forceDist = .1,  		--radius at which forces begin to be applied
+	snapDist = .05,  		--distance at which the part will snap into place
+	stiff = 150				--force factor, how much will be applied
+}
+
+simulation:startInSchedulerThread()
