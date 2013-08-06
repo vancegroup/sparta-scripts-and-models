dd64e266a03206e00b9d5f1ac57c9483dc6866d5
Andrew <admoore@iastate.edu>
2013-08-06 17:20:47 -0500

    Switch to using the haptic service for Virtuose

--- Devices/METaL Haption Tracked.lua
+++ Devices/METaL Haption Tracked.lua
@@ -1,5 +1,6 @@
 virtuose = Manipulators.Haption.Virtuose6D3545{
-	ipaddress = "192.168.1.13",
+	--ipaddress = "192.168.1.13",
+	ipaddress = "127.0.0.1:3131#5002",
 	scale = 1.0,
 	hardwareForceScale = 1.0,
 	torqueScale = 1.0,
